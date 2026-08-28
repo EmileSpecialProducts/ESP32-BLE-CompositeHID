3c29575a236437ef8a7b86ca840fe0c8b9f52fba
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2026-08-28 16:13:43 +0200

    Removed some warnings

--- ArduinoDefines.h
+++ ArduinoDefines.h
@@ -1,4 +1,4 @@
-#ifndef ESP32_ARDUINO_MATH_H
+#if !defined ESP32_ARDUINO_MATH_H
 #define ESP32_ARDUINO_MATH_H
 
 #ifndef LED_BUILTIN
@@ -6,19 +6,41 @@
 #endif
 
 #define abs(x) ((x)>0?(x):-(x))
+#if !defined constrain
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#endif
+#if !defined round
 #define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#endif
+#if !defined radians
 #define radians(deg) ((deg)*DEG_TO_RAD)
+#endif
+#if !defined degrees
 #define degrees(rad) ((rad)*RAD_TO_DEG)
+#endif
+#if !defined sq
 #define sq(x) ((x)*(x))
+#endif
 
+#if !defined lowByte
 #define lowByte(w) ((uint8_t) ((w) & 0xff))
+#endif
+#if !defined highByte
 #define highByte(w) ((uint8_t) ((w) >> 8))
-
+#endif
+#if !defined bitRead
 #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#endif
+#if !defined bitSet
 #define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#endif
+#if !defined bitClear
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#endif
+#if !defined bitToggle
 #define bitToggle(value, bit) ((value) ^= (1UL << (bit)))
+#endif
+#if !defined bitWrite   
 #define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet(value, bit) : bitClear(value, bit))
-
+#endif
 #endif
